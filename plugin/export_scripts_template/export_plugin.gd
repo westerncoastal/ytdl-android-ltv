26ea9d773d5805e94c06002df53159678a112db5
westerncoastal <info@loqoo.delivery>
2026-03-08 17:36:52 -0400

    Refactor AndroidExportPlugin to use base directory

--- plugin/export_scripts_template/export_plugin.gd
+++ plugin/export_scripts_template/export_plugin.gd
@@ -1,23 +1,17 @@
 @tool
 extends EditorPlugin
 
-# A class member to hold the editor export plugin during its lifecycle.
 var export_plugin : AndroidExportPlugin
 
 func _enter_tree():
-	# Initialization of the plugin goes here.
 	export_plugin = AndroidExportPlugin.new()
 	add_export_plugin(export_plugin)
 
-
 func _exit_tree():
-	# Clean-up of the plugin goes here.
 	remove_export_plugin(export_plugin)
 	export_plugin = null
 
-
 class AndroidExportPlugin extends EditorExportPlugin:
-	# TODO: Update to your plugin's name.
 	var _plugin_name = "FlowerPotter"
 
 	func _supports_platform(platform):
@@ -26,19 +20,20 @@ class AndroidExportPlugin extends EditorExportPlugin:
 		return false
 
 	func _get_android_libraries(platform, debug):
+		# This dynamically finds the directory this script is in
+		var base_dir = get_script().get_path().get_base_dir()
 		if debug:
-			return PackedStringArray([_plugin_name + "/bin/debug/" + _plugin_name + "-debug.aar"])
+			return PackedStringArray([base_dir + "/bin/debug/" + _plugin_name + "-debug.aar"])
 		else:
-			return PackedStringArray([_plugin_name + "/bin/release/" + _plugin_name + "-release.aar"])
+			return PackedStringArray([base_dir + "/bin/release/" + _plugin_name + "-release.aar"])
 
 	func _get_android_dependencies(platform, debug):
-		# TODO: Add remote dependices here.
-		if not _supports(platform):
-        	return PackedStringArray()
-    	return PackedStringArray([
-        	"io.github.junkfood02.youtubedl-android:library:0.18.1",
-        	"io.github.junkfood02.youtubedl-android:ffmpeg:0.18.1"
-    	])
+		if not _supports_platform(platform):
+			return PackedStringArray()
+		return PackedStringArray([
+			"io.github.junkfood02.youtubedl-android:library:0.18.1",
+			"io.github.junkfood02.youtubedl-android:ffmpeg:0.18.1"
+		])
 
 	func _get_name():
 		return _plugin_name
